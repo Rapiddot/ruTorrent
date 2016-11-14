bd7ba8d3d840f99fc2a9df4d3b55c08f0768a5e0
Antonios Matsoukas <a.matsoukas@me.com>
2016-11-14 11:01:42 +0200

    Install fileupload, fileshare and mediastream plugins by default Disable ipad plugin by default, for mobile plugin to function correctly

---
+++ plugins/fileshare/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+// limits
+// 0 = unlimited
+$limits['duration'] = 0; 	// maximum duration hours
+$limits['links'] = 0; 	//maximum sharing links per user
+
+// path on domain where a symlink to share.php can be found
+// example: http://mydomain.com/share.php
+$downloadpath = 'varuserhostname'; 
+
+
+?>
